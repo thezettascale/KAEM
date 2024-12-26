d06035f41d25d29414e89d9eb6ec0d38d0815295
PritRaj1 <prithvi.raj1@outlook.com>
2024-12-26 13:56:19 +0000

    deleted rejection

--- src/LV-KAM/mixture_prior.jl
+++ src/LV-KAM/mixture_prior.jl
@@ -51,7 +51,7 @@ function choose_component(alpha, num_samples, q_size, p_size; seed=1)
     rand_vals = rand(rng, Uniform(0,1), q_size, num_samples) 
     
     function categorical_mask(α, rv)
-        
+        """Returns sampled indices from a categorical distribution on alpha."""
         idxs = map(u -> findfirst(x -> x >= u, α), rv)
         idxs = reduce(vcat, idxs)
         idxs = ifelse.(isnothing.(idxs), p_size, idxs)
@@ -61,83 +61,6 @@ function choose_component(alpha, num_samples, q_size, p_size; seed=1)
     
     chosen_components = map(i -> categorical_mask(view(alpha, i, :), view(rand_vals, i, :)), 1:q_size)
     return reduce(hcat, chosen_components) |> device, seed
-end
-
-function rejection_sampler(
-    prior::mix_prior,
-    num_samples::Int, 
-    ps,
-    st;
-    seed::Int=1
-    )
-    """
-    Component-wise rejection sampling for the mixture ebm-prior.
-    p = components of mixture model
-    q = number of mixture models
-
-    Args:
-        prior: The mixture ebm-prior.
-        ps: The parameters of the mixture ebm-prior.
-        st: The states of the mixture ebm-prior.
-
-    Returns:
-        z: The samples from the mixture ebm-prior, (num_samples, q). 
-        seed: The updated seed.
-    """
-
-    previous_samples = zeros(Float32, num_samples) |> device
-    sample_mask = zeros(Float32, num_samples) |> device
-    p_size = prior.fcns_qp[Symbol("$(prior.depth)")].out_dim
-    q_size = prior.fcns_qp[Symbol("1")].in_dim
-    
-    # Categorical component selection (per sample, per outer sum dimension)
-    alpha = cpu_device()(cumsum(softmax(ps[Symbol("α")]; dims=2); dims=2))
-    chosen_components, seed = choose_component(alpha, num_samples, q_size, p_size; seed=seed)
-
-    # Rejection sampling
-    while any(sample_mask .< 5f-1)
-
-        # Draw candidate samples from proposal, i.e. prior
-        seed, rng = next_rng(seed) 
-        z = rand(rng, prior.π_0, num_samples, q_size) |> device # z ~ Q(z)
-
-        # Forward pass of proposal samples through mixture model; p -> q
-        fz_qp = z
-        for i in 1:prior.depth
-            fz_qp = fwd(prior.fcns_qp[Symbol("$i")], ps[Symbol("$i")], st[Symbol("$i")], fz_qp)
-            fz_qp = i == 1 ? reshape(fz_qp, num_samples*q_size, size(fz_qp, 3)) : sum(fz_qp, dims=2)[:, 1, :]
-        end
-        fz_qp = reshape(fz_qp, num_samples, q_size, p_size)
-
-        # Forward pass of grid [0,1] through model
-        f_grid = prior.fcns_qp[Symbol("1")].grid'
-        π_grid = prior.π_pdf(f_grid)
-        grid_size = size(f_grid, 1)
-        for i in 1:prior.depth
-            f_grid = fwd(prior.fcns_qp[Symbol("$i")], ps[Symbol("$i")], st[Symbol("$i")], f_grid)
-            f_grid = i == 1 ? reshape(f_grid, grid_size*q_size, size(f_grid, 3)) : sum(f_grid, dims=2)[:, 1, :] 
-        end
-        f_grid = reshape(f_grid, grid_size, q_size, p_size)
-
-        # Filter chosen components of mixture model, (samples x q)
-        @tullio exp_fz[b, q] := exp(fz_qp[b, q, p]) * chosen_components[b, q, p]
-
-        # Grid search for max_z[ f_{q,c}(z) ] for chosen components
-        @tullio exp_fg[b, g, q] := exp(f_grid[g, q, p]) * π_grid[g, q] * chosen_components[b, q, p]
-        exp_fg = maximum(exp_fg; dims=2)[:,1,:] 
-
-        # Accept or reject
-        seed, rng = next_rng(seed)
-        u_threshold = rand(rng, Uniform(0,1), num_samples, q_size) |> device # u ~ U(0,1)
-        accept_mask = u_threshold .< exp_fz ./ exp_fg
-
-        # Update samples
-        previous_samples = z .* accept_mask .* (1 .- sample_mask) .+ previous_samples .* sample_mask
-        sample_mask = accept_mask .* (1 .- sample_mask) .+ sample_mask
-        clamp.(sample_mask, 0f0, 1f0)
-    end
-
-    return previous_samples, seed
 end
 
 function sample_prior(

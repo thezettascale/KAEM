195ca3a743a4e82abcdb9d9615dca2eda232b755
PritRaj1 <prithvi.raj1@outlook.com>
2025-01-16 17:43:20 +0000

    another fix

--- src/T-KAM/KAN_likelihood.jl
+++ src/T-KAM/KAN_likelihood.jl
@@ -149,7 +149,7 @@ function particle_filter(
     # Only resample when needed 
     verbose && (!all(ESS_bool) && println("Resampling at t=$t"))
     !all(ESS_bool) && return resampler(weights, ESS_bool, B, N; seed=seed)
-    return repeat((1:N)', B, 1), weights, seed
+    return repeat((1:N)', B, 1), seed
 end
 
 function init_KAN_lkhood(

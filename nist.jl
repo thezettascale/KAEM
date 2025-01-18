b8126f3723aea937472bc709ced54f49fec0a1af
PritRaj1 <prithvi.raj1@outlook.com>
2025-01-18 21:06:33 +0000

    added pf too in case

--- nist.jl
+++ nist.jl
@@ -22,10 +22,10 @@ for dataset in datasets
     commit!(conf, "THERMODYNAMIC_INTEGRATION", "num_temps", "-1")
     commit!(conf, "MALA", "use_langevin", "false")
 
-    # Vanilla importance sampling
-    commit!(conf, "THERMODYNAMIC_INTEGRATION", "num_temps", "-1")
-    t = init_trainer(rng, conf, dataset, img_resize=(14,14))
-    train!(t)
+    # # Vanilla importance sampling
+    # commit!(conf, "THERMODYNAMIC_INTEGRATION", "num_temps", "-1")
+    # t = init_trainer(rng, conf, dataset, img_resize=(14,14))
+    # train!(t)
 
     # # MALA Vanilla
     # commit!(conf, "MALA", "use_langevin", "true")
--- src/ML_pipeline/trainer.jl
+++ src/ML_pipeline/trainer.jl
@@ -66,7 +66,8 @@ function init_trainer(rng::AbstractRNG, conf::ConfParse, dataset_name;
 
     N_t = parse(Int, retrieve(conf, "THERMODYNAMIC_INTEGRATION", "num_temps"))
     mala = parse(Bool, retrieve(conf, "MALA", "use_langevin")) ? "MALA" : "importance"
-    model_type = N_t > 1 ? "Thermodynamic" : "Vanilla/$mala"
+    particles = parse(Int, retrieve(conf, "MALA", "num_particles")) > 1 ? "particles" : "MALA"
+    model_type = N_t > 1 ? "Thermodynamic/$particles" : "Vanilla/$mala"
 
     file_loc = isnothing(file_loc) ? "logs/$(model_type)/$(dataset_name)_$(seed)/" : file_loc
     mkpath(file_loc)

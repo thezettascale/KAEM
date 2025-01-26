f27aeb2f9cfe8fef1d34fc17c99efa6b816fe0f7
PritRaj1 <prithvi.raj1@outlook.com>
2025-01-26 13:41:09 +0000

    langevin meh

--- src/tests/langevin_tests.jl
+++ src/tests/langevin_tests.jl
@@ -15,14 +15,12 @@ conf = ConfParse("src/tests/test_conf.ini")
 parse_conf!(conf)
 out_dim = parse(Int, retrieve(conf, "KAN_LIKELIHOOD", "output_dim"))
 
-mkpath("figures/test")
-
 function plot_final_distribution()
-
     Random.seed!(42)
     dataset = randn(quant, 3, 50) 
     commit!(conf, "MALA", "use_langevin", "true")
     commit!(conf, "MALA", "iters", "150")
+    commit!(conf, "TRAINING", "importance_sample_size", "100")
     model = init_T_KAM(dataset, conf)
     x_test = first(model.train_loader) |> device
     ps, st = Lux.setup(Random.GLOBAL_RNG, model)
@@ -30,14 +28,17 @@ function plot_final_distribution()
 
     output = first(model.posterior_sample(model, x_test, ps, st, 1)) |> cpu_device()
     
-    final_samples = vec(output[2, :, 1])
-    histogram(final_samples, bins=50, normalize=true, alpha=0.6, label="Samples")
+    final_samples = vec(output[2, :, :])
     
-    μ, σ = 0.0, 1.0  # Expected Gaussian
+    # Plot the histogram of samples
+    p = histogram(final_samples, bins=50, normalize=true, alpha=0.6, label="Samples")
+    
+    μ, σ = 0.0, 1.0  # Expected Gaussian parameters
     x = range(minimum(final_samples), maximum(final_samples), length=100)
-    plot!(x, pdf.(Normal(μ, σ), x), lw=2, label="Expected Posterior")
-    plot!(legend=:topleft, xlabel="z", ylabel="Density")
-    savefig("figures/test/langevin_convergence.png")
+    plot!(p, x, pdf.(Normal(μ, σ), x), lw=2, label="Expected Posterior")
+    
+    plot!(p, legend=:topleft, xlabel="z", ylabel="Density", title="Langevin Convergence")
+    savefig(p, "figures/test/langevin_convergence.png")
 
     z_prior = first(model.prior.sample_z(model.prior, model.IS_samples, ps.ebm, st.ebm, 1)) |> cpu_device()
 

fc7b95f2ad0cc96b77d19f421326d69f20d4021a
PritRaj1 <prithvi.raj1@outlook.com>
2025-01-29 17:50:44 +0000

    stable

--- src/T-KAM/inversion_sampling.jl
+++ src/T-KAM/inversion_sampling.jl
@@ -39,6 +39,10 @@ function choose_component(
     mask = Array{half_quant}(undef, q_size, p_size, num_samples) 
     Threads.@threads for q in 1:q_size
         i = searchsortedfirst.(Ref(α[q, :]), rand_vals[q, :])
+
+        # If necessary:
+        replace!(i, p_size + 1 => p_size) # Upper bound
+
         mask[q, :, :] = onehotbatch(i, 1:p_size) .|> half_quant
     end
 
@@ -111,6 +115,10 @@ function sample_prior(
             # First trapezium where CDF >= rand_val
             rv = rand_vals[b, q]
             idx = searchsortedfirst(cdf[b, :, q], rv) # Index of upper trapezium bound
+
+            # If necessary:
+            idx = idx == 1 ? 2 : idx
+            idx = idx == grid_size + 1 ? grid_size : idx
 
             # Trapezium bounds
             z1, z2 = grid[idx-1, q], grid[idx, q] 

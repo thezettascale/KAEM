b9a44477d12e1f84117dd0d783204e03fa4cc887
PritRaj1 <prithvi.raj1@outlook.com>
2025-01-30 02:31:43 +0000

    cleaned

--- main_importance.jl
+++ main_importance.jl
@@ -1,19 +1,15 @@
 using ConfParser, Random
 
-conf_nist = ConfParse("nist_config.ini")
-parse_conf!(conf_nist)
+dataset = get(ENV, "DATASET", "MNIST")
+conf = (dataset == "MNIST" || dataset == "FMNIST") ? ConfParse("nist_config.ini") : ConfParse("cnn_config.ini")
+parse_conf!(conf)
 
-conf_cnn = ConfParse("cnn_config.ini")
-parse_conf!(conf_cnn)
-
-ENV["GPU"] = retrieve(conf_nist, "TRAINING", "use_gpu") 
-ENV["FULL_QUANT"] = retrieve(conf_nist, "MIXED_PRECISION", "full_precision")
-ENV["HALF_QUANT"] = retrieve(conf_nist, "MIXED_PRECISION", "reduced_precision")
+ENV["GPU"] = retrieve(conf, "TRAINING", "use_gpu") 
+ENV["FULL_QUANT"] = retrieve(conf, "MIXED_PRECISION", "full_precision")
+ENV["HALF_QUANT"] = retrieve(conf, "MIXED_PRECISION", "reduced_precision")
 
 include("src/ML_pipeline/trainer.jl")
 using .trainer
-
-dataset = get(ENV, "DATASET", "MNIST")
 
 rng = Random.seed!(1)
 

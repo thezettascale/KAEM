4555e042747c9d3c31e359b4ad34b61f8667e989
PritRaj1 <prithvi.raj1@outlook.com>
2025-01-30 02:12:05 +0000

    fixe bash script

--- run.sh
+++ run.sh
@@ -4,6 +4,14 @@ datasets=("MNIST" "FMNIST" "CIFAR10" "SVHN")
 
 for dataset in "${datasets[@]}"; do
     session_name="run_$dataset"
-    tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main.jl > output_${dataset}.log 2>&1"
-    echo "Started tmux session '$session_name' for dataset $dataset"
+    echo "Starting $dataset run..."
+    
+    tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main.jl > output_${dataset}.log 2>&1; tmux kill-session -t $session_name"
+    
+    # Runs sequentially
+    while tmux has-session -t "$session_name" 2>/dev/null; do
+        sleep 5
+    done
+    
+    echo "$dataset completed."
 done

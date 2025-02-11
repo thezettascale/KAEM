dd6f96653e8c3efff3c9f00caf61db8277076c34
PritRaj1 <prithvi.raj1@outlook.com>
2025-02-11 22:17:37 +0000

    darcy conf

--- plt_grid_img.py
+++ plt_grid_img.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 file_path = 'logs/Darcy/informed/seed_1/generated_pressures.h5'
 
 with h5py.File(file_path, 'r') as h5_file:
-    image_data = h5_file['gen_samples'][()] 
+    image_data = h5_file['true_flow'][()] 
 
 # Define grid dimensions
 grid_size = (10, 10) 
@@ -16,6 +16,7 @@ for i in range(grid_size[0] * grid_size[1]):
     ax = axes[row, col]
     
     img = image_data[i, :, :].reshape(32,32)
+    print(max(img.flatten()))
     
     ax.imshow(img)#, cmap='gray')
     ax.axis('off')  

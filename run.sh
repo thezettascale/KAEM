7f280e40981e4d178b18baf81a80f1fcbebaa858
PritRaj1 <prithvi.raj1@outlook.com>
2025-02-15 05:34:45 +0000

    fixed langevin

--- run.sh
+++ run.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-datasets=("MNIST" "FMNIST" "CIFAR10" "SVHN" "PTB")
-
-for dataset in "${datasets[@]}"; do
-    session_name="IS_$dataset"
-    echo "Starting $dataset Importance Sampling run..."
-    
-    # Runs sequentially
-    tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_importance.jl > Vanilla_${dataset}.log 2>&1; tmux kill-session -t $session_name"
-    
-    while tmux has-session -t "$session_name" 2>/dev/null; do
-        sleep 5
-    done
-    
-    echo "$dataset Importance Sampling completed."
-done
-
-# datasets=("CIFAR10" "SVHN")
+# datasets=("MNIST" "FMNIST" "CIFAR10" "SVHN" "PTB")
 
 # for dataset in "${datasets[@]}"; do
-#     session_name="TI_$dataset"
-#     echo "Starting $dataset Thermodynamic Integration run..."
+#     session_name="IS_$dataset"
+#     echo "Starting $dataset Importance Sampling run..."
     
 #     # Runs sequentially
-#     tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_thermodynamic.jl > Thermo_${dataset}.log 2>&1; tmux kill-session -t $session_name"
+#     tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_importance.jl > Vanilla_${dataset}.log 2>&1; tmux kill-session -t $session_name"
     
 #     while tmux has-session -t "$session_name" 2>/dev/null; do
 #         sleep 5
 #     done
     
-#     echo "$dataset Thermodynamic Integration completed."
+#     echo "$dataset Importance Sampling completed."
 # done
+
+datasets=("CIFAR10" "SVHN")
+
+for dataset in "${datasets[@]}"; do
+    session_name="TI_$dataset"
+    echo "Starting $dataset Thermodynamic Integration run..."
+    
+    # Runs sequentially
+    tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_thermodynamic.jl > Thermo_${dataset}.log 2>&1; tmux kill-session -t $session_name"
+    
+    while tmux has-session -t "$session_name" 2>/dev/null; do
+        sleep 5
+    done
+    
+    echo "$dataset Thermodynamic Integration completed."
+done

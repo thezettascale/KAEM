6ebfb8c45fdb7547d0973d9ef7a9a5990f6a00e0
PritRaj1 <prithvi.raj1@outlook.com>
2025-02-14 21:14:31 -0800

    img grid

--- plt_grid_img.py
+++ plt_grid_img.py
@@ -1,8 +1,9 @@
 import h5py
 import matplotlib.pyplot as plt
+import numpy as np
 
 # File path to your HDF5 file
-file_path = 'logs/Vanilla/importance/MNIST_1/generated_images.h5'
+file_path = 'logs/Vanilla/importance/CIFAR10_1/generated_images.h5'
 
 with h5py.File(file_path, 'r') as h5_file:
     image_data = h5_file['samples'][()] 
@@ -15,7 +16,7 @@ for i in range(grid_size[0] * grid_size[1]):
     row, col = divmod(i, grid_size[1])
     ax = axes[row, col]
     
-    img = image_data[i, :, :].reshape(28,28)
+    img = np.transpose(image_data[i, :, :, :], (1, 2, 0))
     print(max(img.flatten()))
     
     ax.imshow(img)#, cmap='gray')

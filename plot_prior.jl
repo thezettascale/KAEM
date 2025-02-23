ec3320d396ebe2d218dae0f1dd9067d63f41ee8b
PritRaj1 <prithvi.raj1@outlook.com>
2025-02-23 12:36:17 -0800

    normalized now

--- plot_prior.jl
+++ plot_prior.jl
@@ -1,4 +1,4 @@
-using JLD2, CUDA, Lux, LuxCUDA, CUDA, ComponentArrays, ConfParser, LaTeXStrings, Makie, GLMakie, Tullio, Random
+using JLD2, CUDA, Lux, LuxCUDA, CUDA, ComponentArrays, ConfParser, LaTeXStrings, Makie, GLMakie, Tullio, Random, Accessors
 
 ENV["GPU"] = "true"
 
@@ -29,18 +29,20 @@ st = saved_data["state"] |> hq |> device
 rng = Random.seed!(1)
 t = init_trainer(rng, conf, dataset_name; file_loc="garbage/")
 prior = t.model.prior
+
 ps = ps.ebm
 st = st.ebm
 t = nothing
 
-z = Float32.(range(prior.fcns_qp[Symbol("1")].grid_range..., length=1000)) |> device
-z = repeat(z', prior.q_size, 1)
+a, b = prior.fcns_qp[Symbol("1")].grid_range
+z = prior.quadrature_method == "trapezium" ? Float32.(range(a,b; length=1000)) |> device : (a + b) ./ 2 .+ (b - a) ./ 2 .* prior.nodes |> device
+z =  prior.quadrature_method == "trapezium" ? repeat(z', prior.q_size, 1) : z
 π_0 = prior.prior_type == "lognormal" ? prior.π_pdf(z, Float32(0.0001)) : prior.π_pdf(z)
 
 f, st = prior_fwd(prior, ps, st, z)
 alpha = softmax(ps[Symbol("α")]; dims=2) |> cpu_device()
-f = f .+ log.(permutedims(π_0[:,:,:], (1, 3, 2))) .- first(log_partition_function(prior, ps, st))
-f = exp.(f) 
+f = exp.(f) .* permutedims(π_0[:,:,:], (1, 3, 2))
+f = softmax(f; dims=3)
 z, f = z |> cpu_device(), f |> cpu_device()
 
 mkpath("figures/results/priors")
@@ -57,7 +59,7 @@ for (q, p) in plot_components
                     show_colorbar = false,
                 )
         hue = alpha[q, p]
-    ax = Makie.Axis(fig[1, 1], title=L"Mixture component, $\frac{\exp(f_{%$q,%$p}) \cdot \pi_0(z)}{\textbf{Z}_{%$q,%$p}}$ \\ Mixture proportions $\alpha_{%$q,%$p} = %$hue$")
+    ax = Makie.Axis(fig[1, 1], title=L"Mixture component, ${\exp(f_{%$q,%$p}) \cdot \pi_0(z)} \; / \; {\textbf{Z}_{%$q,%$p}}$ \\ Mixture proportion $\alpha_{%$q,%$p} = %$hue$")
 
     lines!(ax, z[q, :], f[q, p, :], color=(:red))
     # hidedecorations!(ax)

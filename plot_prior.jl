dff46270e6ae0c55a43b8f1cc015ff4fa486adaf
PritRaj1 <prithvi.raj1@outlook.com>
2025-02-23 14:08:11 -0800

    plot

--- plot_prior.jl
+++ plot_prior.jl
@@ -13,13 +13,14 @@ using .InverseSampling: prior_fwd
 using .Utils: device, half_quant, hq
 using .ebm_mix_prior: log_partition_function
 
-file = "logs/uniform_FFT/DARCY_FLOW_1/saved_model.jld2"
-dataset_name = "DARCY_FLOW"
-conf = ConfParse("config/darcy_flow_config.ini")
+file = "logs/uniform_RBF/MNIST_1/saved_model.jld2"
+dataset_name = "MNIST"
+conf = ConfParse("config/nist_config.ini")
 parse_conf!(conf)
 
 # Components to plot (q, p)
-plot_components = [(1,1)]
+plot_components = [(1,1), (1,2), (1,3), (1,4), (1,5)]
+colours = [:red, :blue, :green, :purple, :orange]
 
 saved_data = load(file)
 
@@ -41,16 +42,15 @@ z =  prior.quadrature_method == "trapezium" ? repeat(z', prior.q_size, 1) : z
 
 f, st = prior_fwd(prior, ps, st, z)
 alpha = softmax(ps[Symbol("α")]; dims=2) |> cpu_device()
-f = exp.(f) .* permutedims(π_0[:,:,:], (1, 3, 2))
-f = softmax(f; dims=3)
+f = exp.(f) .* permutedims(π_0[:,:,:], (1, 3, 2)) ./ exp.(first(log_partition_function(prior, ps, st)))
 z, f = z |> cpu_device(), f |> cpu_device()
 
 mkpath("figures/results/priors")
 
-for (q, p) in plot_components
+for (i, (q, p)) in enumerate(plot_components)
     fig = Makie.Figure(size=(1000, 1000),
                     ffont="Computer Modern", 
-                    fontsize = 20,
+                    fontsize = 40,
                     backgroundcolor = :white,
                     show_axis = false,
                     show_grid = false,
@@ -58,10 +58,11 @@ for (q, p) in plot_components
                     show_legend = false,
                     show_colorbar = false,
                 )
-        hue = alpha[q, p]
+    hue = round(alpha[q, p], digits=3)
     ax = Makie.Axis(fig[1, 1], title=L"Mixture component, ${\exp(f_{%$q,%$p}) \cdot \pi_0(z)} \; / \; {\textbf{Z}_{%$q,%$p}}$ \\ Mixture proportion $\alpha_{%$q,%$p} = %$hue$")
 
-    lines!(ax, z[q, :], f[q, p, :], color=(:red))
+    band!(ax, z[q, :], 0 .* f[q, p, :], f[q, p, :], color=colours[i])
+    lines!(ax, z[q, :], f[q, p, :], color=colours[i])
     # hidedecorations!(ax)
     # hidespines!(ax)
     save("figures/results/priors/$(dataset_name)_$(q)_$(p).png", fig)

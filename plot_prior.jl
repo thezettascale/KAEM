f2a09f8d226b915e5c6a78b812fe7062a5a009a3
PritRaj1 <prithvi.raj1@outlook.com>
2025-03-08 09:46:26 -0800

    need to run darcy again

--- plot_prior.jl
+++ plot_prior.jl
@@ -4,14 +4,12 @@ ENV["GPU"] = "true"
 
 include("src/T-KAM/T-KAM.jl")
 include("src/ML_pipeline/trainer.jl")
-include("src/T-KAM/inversion_sampling.jl")
 include("src/T-KAM/EBM_prior.jl")
 include("src/utils.jl")
 using .T_KAM_model
 using .trainer
-using .InverseSampling: prior_fwd
 using .Utils: device, half_quant, hq
-using .ebm_ebm_prior: log_partition_function
+using .ebm_ebm_prior: prior_fwd
 
 prior_type = "lognormal"
 fcn_type = "RBF"
@@ -60,21 +58,21 @@ for dataset_name in datasets
         "gaussian" => (-3, 3),
     )[prior_type]
 
-    if b == prior.fcns_qp[Symbol("1")].grid_size
-        a, b = prior.fcns_qp[Symbol("1")].grid_range
+    a, b = minimum(st[Symbol("1")].grid; dims=2), maximum(st[Symbol("1")].grid; dims=2)
+    if any(b .== prior.fcns_qp[Symbol("1")].grid_size)
+        a = fill(half_quant(first(prior.fcns_qp[Symbol("1")].grid_range)), size(a)) |> device
+        b = fill(half_quant(last(prior.fcns_qp[Symbol("1")].grid_range)), size(b)) |> device
     end
-    z = prior.quadrature_method == "trapezium" ? Float32.(range(a,b; length=1000)) |> device : (a + b) ./ 2 .+ (b - a) ./ 2 .* prior.nodes |> device
-    z =  prior.quadrature_method == "trapezium" ? repeat(z', prior.q_size, 1) : z
+
+    z = (a + b) ./ 2 .+ (b - a) ./ 2 .* device(prior.nodes)
     π_0 = prior.prior_type == "lognormal" ? prior.π_pdf(z, Float32(0.0001)) : prior.π_pdf(z)
 
     f, st = prior_fwd(prior, ps, st, z)
-    alpha = softmax(ps[Symbol("α")]; dims=2) |> cpu_device()
-    f = exp.(f) .* permutedims(π_0[:,:,:], (1, 3, 2)) 
+    f = exp.(f) .* permutedims(π_0[:,:,:], (3, 1, 2)) 
     z, f, π_0 = z |> cpu_device(), softmax(f;dims=3) |> cpu_device(), softmax(π_0;dims=2) |> cpu_device()
 
     # Components to plot (q, p)
-    sorted_indices = sortperm(vec(alpha), rev=true)[1:3]
-    plot_components = [(div(idx-1, size(alpha,2)) + 1, mod(idx-1, size(alpha,2)) + 1) for idx in sorted_indices]
+    plot_components = [(1, 1), (1, 2), (1, 3)]
     colours = [:red, :blue, :green]
 
     mkpath("figures/results/priors")
@@ -90,14 +88,13 @@ for dataset_name in datasets
                         show_legend = false,
                         show_colorbar = false,
                     )
-        hue = round(alpha[q, p], digits=3)
-        ax = Makie.Axis(fig[1, 1], title=L"Mixture component, ${\exp(f_{%$q,%$p}(z)) \cdot \pi_0(z)} \; / \; {\textbf{Z}_{%$q,%$p}}$ \\ Mixture proportion $\alpha_{%$q,%$p} = %$hue$")
+        ax = Makie.Axis(fig[1, 1], title=L"Mixture component, ${\exp(f_{%$q,%$p}(z)) \cdot \pi_0(z)} \; / \; {\textbf{Z}_{%$q,%$p}}$")
 
-        band!(ax, z[q, :], 0 .* f[q, p, :], f[q, p, :], color=(colours[i], 0.3), label=L"{\exp(f_{%$q,%$p}(z)) \cdot \pi_0(z)}")
-        lines!(ax, z[q, :], f[q, p, :], color=colours[i])
-        band!(ax, z[q, :], 0 .* f[q, p, :], π_0[q,:], color=(:gray, 0.2), label=L"\pi_0(z)")
-        lines!(ax, z[q, :], π_0[q,:], color=(:gray, 0.8))
-        y_min = minimum([minimum(f[q, p, :]), minimum(π_0[q,:])])
+        band!(ax, z[p, :], 0 .* f[q, p, :], f[q, p, :], color=(colours[i], 0.3), label=L"{\exp(f_{%$q,%$p}(z)) \cdot \pi_0(z)}")
+        lines!(ax, z[p, :], f[q, p, :], color=colours[i])
+        band!(ax, z[p, :], 0 .* f[q, p, :], π_0[p,:], color=(:gray, 0.2), label=L"\pi_0(z)")
+        lines!(ax, z[p, :], π_0[p,:], color=(:gray, 0.8))
+        y_min = minimum([minimum(f[q, p, :]), minimum(π_0[p,:])])
         ylims!(ax, y_min, nothing)
         axislegend(ax)
         hidedecorations!(ax)

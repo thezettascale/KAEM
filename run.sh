434565eb995d47b883828811158a38982be808d3
PritRaj1 <prithvi.raj1@outlook.com>
2025-03-11 20:51:29 +0000

    run change

--- run.sh
+++ run.sh
@@ -3,6 +3,22 @@
 datasets=("SVHN" "CIFAR10")
 
 for dataset in "${datasets[@]}"; do
+    session_name="TI_$dataset"
+    echo "Starting $dataset Thermodynamic Integration run..."
+    
+    # Runs sequentially
+    tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_thermodynamic.jl > Thermo_${dataset}.log 2>&1; tmux kill-session -t $session_name"
+    
+    while tmux has-session -t "$session_name" 2>/dev/null; do
+        sleep 5
+    done
+    
+    echo "$dataset Thermodynamic Integration completed."
+done
+
+datasets=("SVHN" "CIFAR10")
+
+for dataset in "${datasets[@]}"; do
     session_name="IS_$dataset"
     echo "Starting $dataset Importance Sampling run..."
     
@@ -16,18 +32,4 @@ for dataset in "${datasets[@]}"; do
     echo "$dataset Importance Sampling completed."
 done
 
-datasets=("SVHN" "CIFAR10")
 
-for dataset in "${datasets[@]}"; do
-    session_name="TI_$dataset"
-    echo "Starting $dataset Thermodynamic Integration run..."
-    
-    # Runs sequentially
-    tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_thermodynamic.jl > Thermo_${dataset}.log 2>&1; tmux kill-session -t $session_name"
-    
-    while tmux has-session -t "$session_name" 2>/dev/null; do
-        sleep 5
-    done
-    
-    echo "$dataset Thermodynamic Integration completed."
-done

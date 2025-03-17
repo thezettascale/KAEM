104f1df2906263792a08cccc79c717339e15cad1
PritRaj1 <prithvi.raj1@outlook.com>
2025-03-17 13:02:44 -0700

    ah whoops. fixed

--- plotting/results/plot_priors.py
+++ plotting/results/plot_priors.py
@@ -12,12 +12,13 @@ plt.rcParams.update({
 
 # File paths to HDF5 files
 file_paths = [
-    'logs/gaussian_RBF/DARCY_FLOW_1/real_images.h5',
-    'logs/gaussian_RBF/DARCY_FLOW_1/generated_images.h5',
-    'logs/gaussian_FFT/DARCY_FLOW_1/generated_images.h5',
+    'logs/gaussian_RBF/MNIST_1/generated_images.h5',
+    'logs/lognormal_RBF/MNIST_1/generated_images.h5',
+    'logs/uniform_RBF/MNIST_1/generated_images.h5',
 ]
 
-titles = ['True Samples', 'RBF', 'FFT']
+# titles = ['Gaussian', 'Lognormal', 'Uniform']
+titles = [r"$\text{Lognormal}(\bm{z}; \; \bm{0}, \bm{1})$", r"$\mathcal{U}(\bm{z}; \; \bm{0}, \bm{1})$", r"$\mathcal{N}(\bm{z}; \; \bm{0}, \bm{1})$"]
 
 # Load generated images
 images = []
@@ -25,7 +26,7 @@ for file_path in file_paths:
     with h5py.File(file_path, 'r') as h5_file:
         images.append(h5_file['samples'][()])
 
-grid_size = (7, 7)  
+grid_size = (13, 13)  
 fig, axes = plt.subplots(grid_size[0], grid_size[1] * 3, figsize=(18, 6))
 
 for dataset_idx, image_set in enumerate(images):
@@ -39,14 +40,14 @@ for dataset_idx, image_set in enumerate(images):
         
         # Use inverted colormap for middle dataset
         if dataset_idx == 1:
-            ax.imshow(images, cmap='cividis')  # gray_r is inverted gray
+            ax.imshow(images, cmap='gray_r')  # gray_r is inverted gray
         else:
-            ax.imshow(images, cmap='viridis')
+            ax.imshow(images, cmap='gray')
             
         ax.axis('off')  
     
-    axes[0, dataset_idx * grid_size[1] + 3].set_title(titles[dataset_idx], fontsize=40, pad=10)
+    axes[0, dataset_idx * grid_size[1] + 6].set_title(titles[dataset_idx], fontsize=40, pad=10)
 
 plt.subplots_adjust(wspace=0, hspace=0)
-plt.savefig('figures/results/RBFvsFFT_gaussian_7x7.png')
+plt.savefig('figures/results/mnist_priors_rbf_13x13.png')
 

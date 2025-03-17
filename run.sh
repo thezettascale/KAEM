d697761d263a2b379f10d69ff68abeb40b9ec913
PritRaj1 <prithvi.raj1@outlook.com>
2025-03-17 18:13:00 +0000

    going to run smaller scale

--- run.sh
+++ run.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-# datasets=("SVHN" "CIFAR10")
-
-# for dataset in "${datasets[@]}"; do
-#     session_name="TI_$dataset"
-#     echo "Starting $dataset Thermodynamic Integration run..."
-    
-#     # Runs sequentially
-#     tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_thermodynamic.jl > Thermo_${dataset}.log 2>&1; tmux kill-session -t $session_name"
-    
-#     while tmux has-session -t "$session_name" 2>/dev/null; do
-#         sleep 5
-#     done
-    
-#     echo "$dataset Thermodynamic Integration completed."
-# done
-
-datasets=("DARCY_FLOW")
+datasets=("SVHN" "CIFAR10")
 
 for dataset in "${datasets[@]}"; do
-    session_name="IS_$dataset"
-    echo "Starting $dataset Importance Sampling run..."
+    session_name="TI_$dataset"
+    echo "Starting $dataset Thermodynamic Integration run..."
     
     # Runs sequentially
-    tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_importance.jl > Vanilla_${dataset}.log 2>&1; tmux kill-session -t $session_name"
+    tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_thermodynamic.jl > Thermo_${dataset}.log 2>&1; tmux kill-session -t $session_name"
     
     while tmux has-session -t "$session_name" 2>/dev/null; do
         sleep 5
     done
     
-    echo "$dataset Importance Sampling completed."
+    echo "$dataset Thermodynamic Integration completed."
 done
+
+# datasets=("DARCY_FLOW")
+
+# for dataset in "${datasets[@]}"; do
+#     session_name="IS_$dataset"
+#     echo "Starting $dataset Importance Sampling run..."
+    
+#     # Runs sequentially
+#     tmux new-session -d -s "$session_name" "DATASET=$dataset julia --threads auto main_importance.jl > Vanilla_${dataset}.log 2>&1; tmux kill-session -t $session_name"
+    
+#     while tmux has-session -t "$session_name" 2>/dev/null; do
+#         sleep 5
+#     done
+    
+#     echo "$dataset Importance Sampling completed."
+# done
 
 

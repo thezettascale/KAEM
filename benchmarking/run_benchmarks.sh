5b4e3c4787e7149452360fd9247616451ec1be99
PritRaj1 <prithvi.raj1@outlook.com>
2025-03-21 17:49:33 -0700

    bench file

--- benchmarking/run_benchmarks.sh
+++ benchmarking/run_benchmarks.sh
@@ -5,11 +5,15 @@ JL_FILES=("benchmarking/latent_dim.jl" "benchmarking/MALA_steps.jl")
 for benchmark in $JL_FILES; 
 do
     echo "Running $benchmark"
-    julia --threads auto "$benchmark" > "$benchmark.txt"
-    if [ $? -ne 0 ]; then
-        echo "Benchmark failed: $benchmark"
-        exit 1
-    fi
+    # julia --threads auto "$benchmark" > "$benchmark.txt"
+
+    tmux new-session -d -s "$benchmark" "julia --threads auto $benchmark > $benchmark.txt"
+
+    while tmux has-session -t "$benchmark" 2>/dev/null; do
+        sleep 5
+    done
+
+    echo "$benchmark completed!"
 done
 
 echo "All benchmarks completed!"

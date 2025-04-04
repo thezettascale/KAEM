22969e268fa778d716b35b0dc873a6d5de2ca0ab
PritRaj1 <prithvi.raj1@outlook.com>
2025-04-04 18:50:44 +0100

    merge

---
+++ plotting/visual_plots/cyclic_annealing.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    'text.usetex': True, 
+    'font.family': 'serif', 
+    'font.serif': ['Compute Modern'], 
+    'axes.unicode_minus': False, 
+    'text.latex.preamble': r'\usepackage{amsmath} \usepackage{amsfonts} \usepackage{amssymb} \usepackage{bm} \newcommand{\probP}{\text{I\kern-0.15em P}}'  
+})
+
+num_param_updates = 100000  # Example value
+num_cycles = 1  # Example value
+initial_p = 2  # Example value
+end_p = 0.5 # Example value
+
+x = np.linspace(0, 2 * np.pi * (num_cycles+0.5), num_param_updates + 1)
+p = initial_p + (end_p - initial_p) * 0.5 * (1 - np.cos(x))
+
+plt.figure(figsize=(4, 4))
+plt.plot(x, p)
+plt.xlabel('Parameter updates', fontsize=16)
+plt.ylabel('p', fontsize=16)
+plt.title('Cyclic p schedule', fontsize=16)
+# plt.show()
+plt.savefig('figures/visual/cyclic_annealing.png', dpi=300)

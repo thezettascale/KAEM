aef4e84e9015d8b1136ebe58908fa85cb3e924f0
PritRaj1 <prithvi.raj1@outlook.com>
2025-05-02 18:07:19 +0100

    mc estimate check

--- src/T-KAM/posterior_sampling/ULA.jl
+++ src/T-KAM/posterior_sampling/ULA.jl
@@ -7,10 +7,16 @@ using Zygote: gradient
 
 include("../../utils.jl")
 include("../EBM_prior.jl")
-include("../KAN_likelihood.jl")
 using .Utils: device, next_rng, half_quant, full_quant, fq
 using .ebm_ebm_prior: log_prior
-using .KAN_likelihood: log_likelihood
+
+function cross_entropy(x::AbstractArray{half_quant}, y::AbstractArray{half_quant}; ε::half_quant=eps(half_quant))
+    return log.(x .+ ε) .* y ./ size(x, 1)
+end
+
+function l2(x::AbstractArray{half_quant}, y::AbstractArray{half_quant}; ε::half_quant=eps(half_quant))
+    return -(x - y).^2
+end
 
 function langevin_sampler(
     m,
@@ -66,10 +72,14 @@ function langevin_sampler(
     seed, rng = next_rng(seed)
     noise = randn(rng, full_quant, Q, P, S, N, T)
 
+    seq = m.lkhood.seq_length > 1
+    ll_fn = seq ? (y_i) -> dropdims(sum(cross_entropy(y_i, x; ε=m.ε); dims=1); dims=1) : (y_i) -> dropdims(sum(l2(y_i, x; ε=m.ε); dims=(1,2,3)); dims=(1,2,3))
+    
     function log_posterior(z_i::AbstractArray{half_quant}, st_i, t_k::half_quant)
         lp, st_ebm = log_prior(m.prior, z_i, ps.ebm, st_i.ebm; ε=m.ε)
-        ll, st_gen, seed = log_likelihood(m.lkhood, ps.gen, st_i.gen, x, z_i; seed=seed, ε=m.ε)
-        logpos = sum(lp) + t_k * sum(ll)
+        x̂, st_gen = m.lkhood.generate_from_z(m.lkhood, ps.gen, st_i.gen, z_i)
+        x̂ = m.lkhood.output_activation(x̂) 
+        logpos = sum(lp) + sum(t_k .* ll_fn(x̂) ./ (2*m.lkhood.σ_llhood^2))
         return logpos .* m.loss_scaling, st_ebm, st_gen
     end
 

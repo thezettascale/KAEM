5a147ce9ad0b7628a59ea579e8f4f0bbc91a215a
PritRaj1 <prithvi.raj1@outlook.com>
2025-06-06 08:01:03 -0700

    logs

--- plt_grid_img.py
+++ plt_grid_img.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # File path to your HDF5 file
-file_path = 'logs/Vanilla/n_z=100/ULA/cnn=true/SVHN_1/generated_images_epoch_1.h5'
+file_path = 'logs/Vanilla/n_z=100/ULA/cnn=true/CIFAR10_1/generated_images_epoch_20.h5'
 
 with h5py.File(file_path, 'r') as h5_file:
     image_data = h5_file['samples'][()] 
 
 # Define grid dimensions
-grid_size = (8, 8) 
-fig, axes = plt.subplots(grid_size[0], grid_size[1], figsize=(8, 8))
+grid_size = (20, 20) 
+fig, axes = plt.subplots(grid_size[0], grid_size[1], figsize=(10, 10))
 
 for i in range(grid_size[0] * grid_size[1]):
     row, col = divmod(i, grid_size[1])

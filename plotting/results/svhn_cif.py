90288ec5628995afeff3468d892f022607ca8da9
PritRaj1 <prithvi.raj1@outlook.com>
2025-06-15 18:24:08 -0700

    figs

--- plotting/results/svhn_cif.py
+++ plotting/results/svhn_cif.py
@@ -18,37 +18,43 @@ GRID_SIZES = [10, 20]
 for prior in priors:
     for dataset in datasets:
         for grid_size in GRID_SIZES:
-            gen_file_path = f'logs/Vanilla/n_z=100/ULA{prior}/cnn=true/{dataset}_1/generated_images.h5'
-            real_file_path = f'logs/Thermodynamic{prior}/n_z=100/{dataset}_1/generated_images.h5'
+            # MLE/ULA images
+            mle_file_path = f'logs/Vanilla/n_z=100/ULA{prior}/cnn=true/{dataset}_1/generated_images.h5'
+            # SE/ULA images
+            se_file_path = f'logs/Thermodynamic{prior}/n_z=100/{dataset}_1/generated_images.h5'
 
-            titles = ["MLE / ULA", "SE / ULA"]
-            images = []
-            with h5py.File(gen_file_path, 'r') as h5_file:
-                images.append(h5_file['samples'][()])
-            with h5py.File(real_file_path, 'r') as h5_file:
-                images.append(h5_file['samples'][()])
+            # Plot MLE/ULA grid
+            with h5py.File(mle_file_path, 'r') as h5_file:
+                mle_images = h5_file['samples'][()]
             
-            fig, axes = plt.subplots(grid_size, grid_size * 2, figsize=(16, 8))
-            
-            for dataset_idx, image_set in enumerate(images):
-                for i in range(grid_size * grid_size):
-                    row, col = divmod(i, grid_size)
-                    col += dataset_idx * grid_size  
-                    ax = axes[row, col]
-                    
-                    img = np.transpose(image_set[i, :, :, :], (1, 2, 0))
-                    
-                    ax.imshow(img)
-                    ax.axis('off')
+            fig, axes = plt.subplots(grid_size, grid_size, figsize=(8, 8))
+            for i in range(grid_size * grid_size):
+                row, col = divmod(i, grid_size)
+                ax = axes[row, col]
                 
-                axes[0, dataset_idx * grid_size + grid_size // 2].set_title(titles[dataset_idx], fontsize=40, pad=10)
-
-            # Add spacing between the two image sets (after 5th column)
+                img = np.transpose(mle_images[i, :, :, :], (1, 2, 0))
+                ax.imshow(img)
+                ax.axis('off')
+            
+            axes[0, grid_size // 2].set_title("MLE / ULA", fontsize=40, pad=10)
             plt.subplots_adjust(wspace=0, hspace=0)
-            for i in range(grid_size):
-                axes[i, 4].set_axis_off()
-                axes[i, 5].set_axis_off()
+            plt.savefig(f'figures/results/{dataset}{prior}_{grid_size}_mle.png')
+            plt.close()
 
-            plt.savefig(f'figures/results/{dataset}{prior}_{grid_size}.png')
-            # plt.show()
+            # Plot SE/ULA grid
+            with h5py.File(se_file_path, 'r') as h5_file:
+                se_images = h5_file['samples'][()]
+            
+            fig, axes = plt.subplots(grid_size, grid_size, figsize=(8, 8))
+            for i in range(grid_size * grid_size):
+                row, col = divmod(i, grid_size)
+                ax = axes[row, col]
+                
+                img = np.transpose(se_images[i, :, :, :], (1, 2, 0))
+                ax.imshow(img)
+                ax.axis('off')
+            
+            axes[0, grid_size // 2].set_title("SE / ULA", fontsize=40, pad=10)
+            plt.subplots_adjust(wspace=0, hspace=0)
+            plt.savefig(f'figures/results/{dataset}{prior}_{grid_size}x{grid_size}.png')
             plt.close()

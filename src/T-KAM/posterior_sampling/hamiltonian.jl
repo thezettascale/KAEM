edc116da35143bcf9e365644e9bf4029f96b67c7
PritRaj1 <prithvi.raj1@outlook.com>
2025-07-12 20:37:42 -0700

    fix

--- src/T-KAM/posterior_sampling/hamiltonian.jl
+++ src/T-KAM/posterior_sampling/hamiltonian.jl
@@ -13,20 +13,10 @@ function position_update(
     ∇z::AbstractArray{U},
     M::AbstractArray{U},
     η::AbstractArray{U},
-    ) where {U<:full_quant}
-    nd = ndims(z)
-    η_half = η ./ 2
-
-    if nd == 4
-        y = momentum .+ (η_half[reshape(1,1,:,:)] .* ∇z) ./ M
-        ẑ = z .+ (η[reshape(1,1,:,:)] .* y) ./ M
-    elseif nd == 3
-        y = momentum .+ (η_half[reshape(1,1,:)] .* ∇z) ./ M
-        ẑ = z .+ (η[reshape(1,1,:)] .* y) ./ M
-    else
-        throw(ArgumentError("position_update only supports 3D or 4D arrays"))
-    end
-
+) where {U<:full_quant}
+    η = reshape(η, 1, 1, size(η)...)
+    y = momentum .+ (η ./ 2) .* ∇z ./ M
+    ẑ = z .+ η .* y ./ M
     return y, ẑ
 end
 
@@ -34,20 +24,9 @@ function momentum_update(
     y::AbstractArray{U},
     ∇ẑ::AbstractArray{U},
     M::AbstractArray{U},
-    η::AbstractArray{U},
-    ) where {U<:full_quant}
-    nd = ndims(y)
-    η_half = η ./ 2
-
-    if nd == 4
-        y_out = y .+ (η_half[reshape(1,1,:,:)] .* ∇ẑ) ./ M
-    elseif nd == 3
-        y_out = y .+ (η_half[reshape(1,1,:)] .* ∇ẑ) ./ M
-    else
-        throw(ArgumentError("momentum_update only supports 3D or 4D arrays"))
-    end
-    
-    return y_out
+    η::AbstractArray{U}
+) where {U<:full_quant}
+    return y .+ (reshape(η, 1, 1, size(η)...) ./ 2) .* ∇ẑ ./ M
 end
 
-end
+end 

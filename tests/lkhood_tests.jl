f7a47cd4ad2239035f4e49cad26bead6d8a95141
PritRaj1 <prithvi.raj1@outlook.com>
2025-07-20 07:29:06 +0100

    likelihood passed on cpu

--- tests/lkhood_tests.jl
+++ tests/lkhood_tests.jl
@@ -1,15 +1,17 @@
-using Test, Random, LinearAlgebra, Lux, ConfParser, ComponentArrays
+using Test, Random, LinearAlgebra, Lux, ConfParser, ComponentArrays, Enzyme, CUDA
 
-ENV["GPU"] = true
+ENV["GPU"] = false
 ENV["FULL_QUANT"] = "FP32"
 ENV["HALF_QUANT"] = "FP32"
 
 include("../src/T-KAM/ebm/ebm_model.jl")
 include("../src/T-KAM/gen/gen_model.jl")
+include("../src/T-KAM/gen/loglikelihoods.jl")
 include("../src/utils.jl")
 using .EBM_Model: init_EbmModel, EbmModel
 using .GeneratorModel
-using .Utils
+using .Utils: device, half_quant, full_quant
+using .GeneratorModel: log_likelihood_IS
 
 conf = ConfParse("tests/test_conf.ini")
 parse_conf!(conf)
@@ -40,6 +42,53 @@ function test_generate()
     z = first(wrap.prior.sample_z(wrap, b_size, ps, st, Random.default_rng()))
     x, _ = lkhood.generate_from_z(lkhood, ps.gen, st.gen, z)
     @test size(x) == (32, 32, 1, b_size)
+end
+
+function test_logllhood()
+    Random.seed!(42)
+    lkhood = init_GenModel(conf, (32, 32, 1))
+    gen_ps, gen_st = Lux.setup(Random.default_rng(), lkhood)
+
+    ps = (ebm = ebm_ps, gen = gen_ps) |> ComponentArray |> device
+    st = (ebm = ebm_st, gen = gen_st) |> device
+
+    x = randn(half_quant, 32, 32, 1, b_size) |> device
+    z = first(wrap.prior.sample_z(wrap, b_size, ps, st, Random.default_rng()))
+    logllhood, _ = log_likelihood_IS(z, x, lkhood, ps.gen, st.gen)
+    @test size(logllhood) == (b_size, b_size)
+end
+
+function test_grad_llhood()
+    Random.seed!(42)
+    lkhood = init_GenModel(conf, (32, 32, 1))
+    gen_ps, gen_st = Lux.setup(Random.default_rng(), lkhood)
+
+    ps = (ebm = ebm_ps, gen = gen_ps) |> ComponentArray |> device
+    st = (ebm = ebm_st, gen = gen_st) |> device
+
+    x = randn(half_quant, 32, 32, 1, b_size) |> device
+    z = first(wrap.prior.sample_z(wrap, b_size, ps, st, Random.default_rng()))
+    grads = Enzyme.make_zero(ps.gen)
+
+    closure = (z_i, x_i, ll, ps_gen, st_gen) -> begin
+        logllhood, _ = log_likelihood_IS(z_i, x_i, ll, ps_gen, st_gen)
+        return sum(logllhood)
+    end
+
+    CUDA.@fastmath Enzyme.autodiff(
+        Enzyme.set_runtime_activity(Enzyme.Reverse), 
+        closure, 
+        Enzyme.Active,
+        Enzyme.Const(z),
+        Enzyme.Const(x),
+        Enzyme.Const(lkhood),
+        Enzyme.Duplicated(ps.gen, grads),
+        Enzyme.Const(st.gen),
+    )
+
+    @test !all(iszero, grads)
+    @test !any(isnan, grads)
+    println(grads)
 end
 
 function test_cnn_generate()
@@ -75,23 +124,10 @@ function test_seq_generate()
     commit!(conf, "SEQ", "sequence_length", "1")
 end
 
-function test_logllhood()
-    Random.seed!(42)
-    lkhood = init_GenModel(conf, (out_dim, out_dim, 1))
-    gen_ps, gen_st = Lux.setup(Random.default_rng(), lkhood)
-
-    ps = (ebm = ebm_ps, gen = gen_ps) |> ComponentArray |> device
-    st = (ebm = ebm_st, gen = gen_st) |> device
-
-    x = randn(half_quant, out_dim, out_dim, 1, b_size) |> device
-    z = first(wrap.prior.sample_z(wrap, b_size, ps, st, Random.default_rng()))
-    logllhood, _ = log_likelihood_IS(z, x, lkhood, ps.gen, st.gen)
-    @test size(logllhood) == (b_size, b_size)
-end
-
 @testset "KAN Likelihood Tests" begin
     test_generate()
+    test_logllhood()
+    test_grad_llhood()
     test_cnn_generate()
     test_seq_generate()
-    test_logllhood()
 end

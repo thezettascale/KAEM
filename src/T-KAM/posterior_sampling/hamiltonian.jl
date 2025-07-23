bf65d97df61ee92ec5a7a10fccf4023bfd1892a3
PritRaj1 <prithvi.raj1@outlook.com>
2025-07-23 20:19:19 +0100

    stencils maxed @ 3 dim

--- src/T-KAM/posterior_sampling/hamiltonian.jl
+++ src/T-KAM/posterior_sampling/hamiltonian.jl
@@ -2,39 +2,32 @@ module HamiltonianDynamics
 
 export position_update, momentum_update, leapfrop_proposal
 
-using CUDA, KernelAbstractions, Tullio, Lux, LuxCUDA, ComponentArrays, ParallelStencil
+using CUDA, KernelAbstractions, Tullio, Lux, LuxCUDA, ComponentArrays
 
 include("../../utils.jl")
 using .Utils: full_quant, half_quant
 
-@static if CUDA.has_cuda() && parse(Bool, get(ENV, "GPU", "false"))
-    @init_parallel_stencil(CUDA, full_quant, 4)
-else
-    @init_parallel_stencil(Threads, full_quant, 4)
-end
-
-# Half-step momentum update (p* = p + (eps/2)M^{-1/2}grad) and full step position update
-@parallel_indices (q, p, s, t) function position_update!(
-    p::AbstractArray{U},
-    ẑ::AbstractArray{U},
+function position_update(
+    z::AbstractArray{U},
     momentum::AbstractArray{U},
     ∇z::AbstractArray{U},
     M::AbstractArray{U},
     η::AbstractArray{U},
-)::Nothing where {U<:full_quant}
-    p[q, p, s, t] = momentum[q, p, s, t] + (η[s, t] / 2) * ∇z[q, p, s, t] / M[q, p, s, t]
-    ẑ[q, p, s, t] = ẑ[q, p, s, t] + η[s, t] * p[q, p, s, t] / M[q, p, s, t]
-    return nothing
+)::Tuple{AbstractArray{U},AbstractArray{U}} where {U<:full_quant}
+    ηr = reshape(η, 1, 1, size(η)...)
+    y = @. momentum + (ηr / 2) * ∇z / M
+    @. z = z + ηr * y / M
+    return y, z
 end
 
-# Half-step momentum update (p* = p + (eps/2)M^{-1/2}grad)
-@parallel_indices (q, p, s, t) function momentum_update!(
-    p::AbstractArray{U},
+function momentum_update!(
+    y::AbstractArray{U},
     ∇ẑ::AbstractArray{U},
     M::AbstractArray{U},
     η::AbstractArray{U},
 )::Nothing where {U<:full_quant}
-    p[q, p, s, t] = p[q, p, s, t] + (η[s, t] / 2) * ∇ẑ[q, p, s, t] / M[q, p, s, t]
+    ηr = reshape(η, 1, 1, size(η)...)
+    @. y + (ηr / 2) * ∇ẑ / M
     return nothing
 end
 
@@ -66,23 +59,14 @@ function leapfrop_proposal(
     x'(x,y*)  = x  + eps M^{-1/2}y*
     y'(x',y*) = y* + (eps/2)M^{-1/2}grad(log pi)(x')
     """
-    Q, P, S = size(z)[1:3]...
+    # # Half-step momentum update (p* = p + (eps/2)M^{-1/2}grad) and full step position update
+    p, ẑ = position_update(z, momentum, ∇z, M, η)
 
-    if ndims(z) == 3
-        p = @zeros(Q, P, S, 1)
-        ẑ = @zeros(Q, P, S, 1)
-        @parallel (1:Q, 1:P, 1:S, 1:1) position_update!(p, ẑ, momentum[:,:,:,:], ∇z[:,:,:,:], M[:,:,:,:], η[:,:])
-        logpos_ẑ, ∇ẑ, st_kan, st_lux = logpos_withgrad(T.(ẑ), x, temps, model, ps, st_kan, st_lux)
-        @parallel (1:Q, 1:P, 1:S, 1:1) momentum_update!(p, ∇ẑ, M[:,:,:,:], η[:,:])
-        ẑ, logpos_ẑ, ∇ẑ, -p = ẑ[:,:,:,1], logpos_ẑ[:,1], ∇ẑ[:,:,:,1], p[:,:,:,1]
-    else
-        num_temps = size(η, 2)
-        p = @zeros(Q, P, S, num_temps)
-        ẑ = @zeros(Q, P, S, num_temps)
-        @parallel (1:Q, 1:P, 1:S, 1:num_temps) position_update!(p, ẑ, momentum, ∇z, M, η)
-        logpos_ẑ, ∇ẑ, st_kan, st_lux = logpos_withgrad(T.(ẑ), x, temps, model, ps, st_kan, st_lux)
-        @parallel (1:Q, 1:P, 1:S, 1:num_temps) momentum_update!(p, ∇ẑ, M, η)
-    end
+    # Get gradient at new position
+    logpos_ẑ, ∇ẑ, st_kan, st_lux = logpos_withgrad(T.(ẑ), x, temps, model, ps, st_kan, st_lux)
+
+    # Half-step momentum update (p* = p + (eps/2)M^{-1/2}grad)
+    momentum_update!(p, ∇ẑ, M, η)
 
     # Hamiltonian difference for transformed momentum
     # H(x,y) = -log(pi(x)) + (1/2)||p||^2 since p ~ N(0,I)

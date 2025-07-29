dde50e6003d67a4fa39a48bcf1111b96778ad075
PritRaj1 <prithvi.raj1@outlook.com>
2025-07-29 05:08:18 +0100

    deferred

--- tests/function_tests.jl
+++ tests/function_tests.jl
@@ -54,7 +54,7 @@ function test_derivative()
         return sum(y)
     end
 
-    Enzyme.autodiff(
+    Enzyme.autodiff_deferred(
         Enzyme.Reverse,
         Enzyme.Const(closured_f),
         Enzyme.Active,

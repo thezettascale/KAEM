e801e9448a4d93f40330a9655043e236675733a3
PritRaj1 <prithvi.raj1@outlook.com>
2025-09-23 00:14:26 +0100

    perceptual fix

--- src/T-KAM/gen/losses.jl
+++ src/T-KAM/gen/losses.jl
@@ -8,7 +8,8 @@ using CUDA, KernelAbstractions, Tullio
 
 perceptual_loss = parse(Bool, get(ENV, "PERCEPTUAL", "true"))
 feature_extractor = nothing
-feature_ckpts = [2, 5, 9, 12]
+style_lyrs = [2, 5, 9, 12]
+content_lyrs = [9]
 if perceptual_loss
     using Metalhead: VGG
     feature_extractor = VGG(16; pretrain = true).layers[1][1:12] |> pu # Conv layers only, (rest is classifier)
@@ -94,6 +95,20 @@ function l2_MALA(
     return ll ./ scale
 end
 
+function gramm_loss(
+    x::AbstractArray{T,4},
+    x̂::AbstractArray{T,4},
+    scale::T,
+)::AbstractArray{T,1} where {T<:half_quant}
+    H, W, C, B = size(x)
+    real = reshape(x, H * W, C, B)
+    fake = reshape(x̂, H * W, C, B)
+    @tullio G_real[c1, c2, b] := real[p, c1, b] * real[p, c2, b]
+    @tullio G_fake[c1, c2, b] := fake[p, c1, b] * fake[p, c2, b]
+    @tullio ll[b] := - (G_real[c1, c2, b] - G_fake[c1, c2, b]) ^ 2
+    return ll ./ scale
+end
+
 function feature_loss(
     x::AbstractArray{T,4},
     x̂::AbstractArray{T,4},
@@ -104,13 +119,20 @@ function feature_loss(
     loss = l2_MALA(x, x̂, ε, scale, perceptual_scale)
     real_features, fake_features = x, x̂
     for (idx, layer) in enumerate(feature_extractor)
+        scale_at_lyr = prod(size(real_features)[1:3]) * scale
         real_features, fake_features = layer(real_features), layer(fake_features)
         loss =
-            (idx in feature_ckpts) ?
+            (idx in style_lyrs) ?
+            perceptual_scale .* gramm_loss(real_features, fake_features, scale_at_lyr) +
+            loss : loss
+
+        loss =
+            (idx in content_lyrs) ?
             perceptual_scale .*
-            l2_MALA(real_features, fake_features, ε, scale, perceptual_scale) + loss : loss
+            l2_MALA(real_features, fake_features, ε, scale_at_lyr, perceptual_scale) +
+            loss : loss
     end
-    return loss ./ length(feature_ckpts)
+    return loss
 end
 
 function MALA_loss(

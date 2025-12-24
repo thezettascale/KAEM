e08935e2fef53821db06f049c027bd5ec9d7cc85
PritRaj1 <prithvi.raj1@outlook.com>
2025-12-24 06:35:09 +0000

    logs

--- src/KAEM/ebm/ebm_model.jl
+++ src/KAEM/ebm/ebm_model.jl
@@ -82,8 +82,8 @@ function init_EbmModel(conf::ConfParse; rng::AbstractRNG = Random.default_rng())
     prior_domain = Dict(
         "ebm" => grid_range,
         "learnable_gaussian" => grid_range,
-        "lognormal" => [0.0f0, 6.0f0],
-        "gaussian" => [-2.0f0, 2.0f0],
+        "lognormal" => [0.0f0, 4.0f0],
+        "gaussian" => [-1.5f0, 1.5f0],
         "uniform" => [0.0f0, 1.0f0],
     )[prior_type]
 

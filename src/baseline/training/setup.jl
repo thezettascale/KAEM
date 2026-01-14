1f2d4bd979cef54ce5a671e3dd88c6f9547dc1f2
PritRaj1 <prithvi.raj1@outlook.com>
2026-01-13 16:56:14 -0800

    ddpm trial

--- src/baseline/training/setup.jl
+++ src/baseline/training/setup.jl
@@ -93,11 +93,8 @@ function prep_ddpm(
 
     t_idx = rand(rng, 1:model.num_timesteps, model.batch_size)
     t = Float32.(t_idx) |> pu
-    sqrt_alpha_gathered = model.sqrt_alphas_cumprod_vec[t_idx]
-    sqrt_one_minus_alpha_gathered = model.sqrt_one_minus_alphas_cumprod_vec[t_idx]
-    broadcast_shape = (ones(Int, length(model.x_shape))..., model.batch_size)
-    sqrt_alpha = collect(reshape(sqrt_alpha_gathered, broadcast_shape)) |> pu
-    sqrt_one_minus_alpha = collect(reshape(sqrt_one_minus_alpha_gathered, broadcast_shape)) |> pu
+    sqrt_alpha = model.sqrt_alphas_cumprod_vec[t_idx] |> pu
+    sqrt_one_minus_alpha = model.sqrt_one_minus_alphas_cumprod_vec[t_idx] |> pu
     noise = randn(rng, T, model.x_shape..., model.batch_size) |> pu
     train_step = DDPMTrainStep(model)
 

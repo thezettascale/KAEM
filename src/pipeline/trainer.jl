53019ee34d19dc20385fdf1a8826a1bcca809286
PritRaj1 <prithvi.raj1@outlook.com>
2026-01-20 17:00:06 -0800

    conf

--- src/pipeline/trainer.jl
+++ src/pipeline/trainer.jl
@@ -524,7 +524,7 @@ function train!(t::KAEM_trainer; train_idx::Int = 1, trial = nothing)
     (t.save_model && !t.img_tuning) && jldsave(
         t.model.file_loc * "saved_model.jld2";
         params = Array(t.ps),
-        kan_state = Array(st_kan),
+        kan_state = Array(t.st_kan),
         lux_state = t.st_lux |> cpu_device(),
         train_idx = train_idx,
     )

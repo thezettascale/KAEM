4bbc64445dba52fbbf88e828e57246c0e353b0e4
PritRaj1 <prithvi.raj1@outlook.com>
2026-03-07 23:25:10 -0800

    fix

--- regen_celeba.jl
+++ regen_celeba.jl
@@ -17,17 +17,19 @@ using .trainer
 using .trainer: seed_rand
 
 rng = Random.MersenneTwister(1)
-t = init_trainer(rng, conf, dataset; img_resize=(64, 64), file_loc=file_loc, save_model=false)
+t = init_trainer(rng, conf, dataset; img_resize = (64, 64), file_loc = file_loc, save_model = false)
 
-# Load saved model
+# Load saved model - reconstruct ComponentArray using model's parameter axes
 saved_data = load(file_loc * "saved_model.jld2")
-t.ps = convert(ComponentArray, saved_data["params"]) |> pu
-t.st_kan = convert(NamedTuple, saved_data["kan_state"]) |> pu
-t.st_lux = convert(NamedTuple, saved_data["lux_state"]) |> pu
+ps_flat = saved_data["params"] .|> Float32
+ps_template = Lux.initialparameters(rng, t.model)
+t.ps = ComponentArray(ps_flat, getaxes(ps_template)) |> pu
+t.st_kan = saved_data["kan_state"] |> pu
+t.st_lux = saved_data["lux_state"] |> pu
 
 # Compile generation function
 println("Compiling gen...")
-t.st_rng = seed_rand(t.model; rng=t.rng)
+t.st_rng = seed_rand(t.model; rng = t.rng)
 gen_compiled = Reactant.@compile t.model(
     t.ps,
     t.st_kan,
@@ -40,7 +42,7 @@ println("gen compiled.")
 num_batches = t.num_generated_samples ÷ t.model.batch_size
 concat_dim = length(t.model.lkhood.x_shape) + 1
 
-t.st_rng = seed_rand(t.model; rng=t.rng)
+t.st_rng = seed_rand(t.model; rng = t.rng)
 first_batch, _, _ = gen_compiled(
     t.ps,
     t.st_kan,
@@ -54,7 +56,7 @@ sizehint!(batches_to_cat, num_batches)
 push!(batches_to_cat, first_batch)
 
 for i in 2:num_batches
-    t.st_rng = seed_rand(t.model; rng=t.rng)
+    t.st_rng = seed_rand(t.model; rng = t.rng)
     batch, _, _ = gen_compiled(
         t.ps,
         t.st_kan,
@@ -65,7 +67,7 @@ for i in 2:num_batches
     println("Batch $i/$num_batches done")
 end
 
-gen_data = cat(batches_to_cat..., dims=concat_dim)
+gen_data = cat(batches_to_cat..., dims = concat_dim)
 
 # Save
 out_path = file_loc * "generated_images.h5"

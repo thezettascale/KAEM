49341f1bef51892e5f72487d873e66aa5b3dcf9d
PritRaj1 <prithvi.raj1@outlook.com>
2026-03-07 23:18:01 -0800

    script to regen lost samples

---
+++ regen_celeba.jl
@@ -0,0 +1,79 @@
+using ConfParser, Random, JLD2, ComponentArrays, HDF5, Lux, Reactant
+
+dataset = "CELEBA"
+file_loc = "logs/Vanilla/CELEBA/ULA/mixture/"
+
+conf = ConfParse("config/celeba_config.ini")
+parse_conf!(conf)
+
+# Match main.jl vanilla mode settings
+commit!(conf, "THERMODYNAMIC_INTEGRATION", "num_temps", "-1")
+
+ENV["DEVICE"] = retrieve(conf, "TRAINING", "device")
+ENV["PERCEPTUAL"] = retrieve(conf, "TRAINING", "use_perceptual_loss")
+
+include("src/pipeline/trainer.jl")
+using .trainer
+using .trainer: seed_rand
+
+rng = Random.MersenneTwister(1)
+t = init_trainer(rng, conf, dataset; img_resize=(64, 64), file_loc=file_loc, save_model=false)
+
+# Load saved model
+saved_data = load(file_loc * "saved_model.jld2")
+t.ps = convert(ComponentArray, saved_data["params"]) |> pu
+t.st_kan = convert(NamedTuple, saved_data["kan_state"]) |> pu
+t.st_lux = convert(NamedTuple, saved_data["lux_state"]) |> pu
+
+# Compile generation function
+println("Compiling gen...")
+t.st_rng = seed_rand(t.model; rng=t.rng)
+gen_compiled = Reactant.@compile t.model(
+    t.ps,
+    t.st_kan,
+    Lux.testmode(t.st_lux),
+    t.st_rng,
+)
+println("gen compiled.")
+
+# Generate samples
+num_batches = t.num_generated_samples ÷ t.model.batch_size
+concat_dim = length(t.model.lkhood.x_shape) + 1
+
+t.st_rng = seed_rand(t.model; rng=t.rng)
+first_batch, _, _ = gen_compiled(
+    t.ps,
+    t.st_kan,
+    Lux.testmode(t.st_lux),
+    t.st_rng,
+)
+first_batch = Array(first_batch)
+
+batches_to_cat = Vector{typeof(first_batch)}()
+sizehint!(batches_to_cat, num_batches)
+push!(batches_to_cat, first_batch)
+
+for i in 2:num_batches
+    t.st_rng = seed_rand(t.model; rng=t.rng)
+    batch, _, _ = gen_compiled(
+        t.ps,
+        t.st_kan,
+        Lux.testmode(t.st_lux),
+        t.st_rng,
+    )
+    push!(batches_to_cat, Array(batch))
+    println("Batch $i/$num_batches done")
+end
+
+gen_data = cat(batches_to_cat..., dims=concat_dim)
+
+# Save
+out_path = file_loc * "generated_images.h5"
+println("Saving $(size(gen_data)) to $out_path")
+try
+    h5write(out_path, "samples", gen_data)
+catch
+    rm(out_path)
+    h5write(out_path, "samples", gen_data)
+end
+println("Done.")
